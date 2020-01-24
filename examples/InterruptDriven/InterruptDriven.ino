d10642e3996c4f44a8234c6f9e82691df6f085c0
arturo182 <arturo182@tlen.pl>
2020-01-24 23:20:14 +0100

    Move the pin to a variable

--- examples/InterruptDriven/InterruptDriven.ino
+++ examples/InterruptDriven/InterruptDriven.ino
@@ -3,6 +3,7 @@
 BBQ10Keyboard keyboard;
 volatile bool dataReady = false;
 
+const int interruptPin = 5;
 
 void KeyIsr(void)
 {
@@ -15,7 +16,7 @@ void setup()
   Wire.begin();
 
   keyboard.begin();
-  keyboard.attachInterrupt(5, KeyIsr);
+  keyboard.attachInterrupt(interruptPin, KeyIsr);
   keyboard.setBacklight(0.5f);
 }
 
@@ -30,7 +31,7 @@ void loop()
     state = "held down";
   else if (key.state == BBQ10Keyboard::StateRelease)
     state = "released";
-  
+
   Serial.printf("key: '%c' (dec %d, hex %02x) %s\r\n", key.key, key.key, key.key, state.c_str());
 
   // pressing 'b' turns off the backlight and pressing Shift+b turns it on
